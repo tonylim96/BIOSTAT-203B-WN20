7d89a6f5319dde9230057f6cadc77b11bf633e6f
tonylim <tonylim@ucla.edu>
2020-01-25 04:10:43 +0000

    Completed Q4.2

--- hw1/autoSim.R
+++ hw1/autoSim.R
@@ -1,11 +1,19 @@
 # autoSim.R
-
-nVals = seq(100, 500, by = 100)
-for (n in nVals) {
-  oFile <- paste("n", n, ".txt", sep="")
-  sysCall <- paste("nohup Rscript runSim.R n=", n, " > ", oFile, sep="")
-  system(sysCall)
-  print(paste("sysCall=", sysCall, sep=""))
+for (arg in commandArgs(TRUE)) {
+  eval(parse(text=arg))
 }
 
-
+nVals = seq(100, 500, by = 100)
+distTypes = c("gaussian", "t1", "t5")
+seed = 203
+rep = 50
+for (n in nVals) {
+  for (i in distTypes) {
+    oFile <- paste(shQuote(i), "_dist_n", n, ".txt", sep="")
+    arg = paste("n=", n,
+                " seed=", seed, " rep=", rep, " dist=", shQuote(shQuote(i)), sep="")
+    sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile, sep="")
+    system(sysCall)
+    print(paste("sysCall=", sysCall, sep=""))
+  }
+}
